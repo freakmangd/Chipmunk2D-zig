3ab085e502e2a3e983c5eb4dde1bc5f4e2fff864
freakmangd <53349189+freakmangd@users.noreply.github.com>
2024-04-08 20:09:59 -0400

    switched from GitRepoStep to package manager

--- build.zig
+++ build.zig
@@ -1,45 +1,10 @@
 const std = @import("std");
-const GitRepoStep = @import("GitRepoStep.zig");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = addChipmunk(b, target, optimize);
-    b.installArtifact(lib);
-
-    const example = b.addExecutable(.{
-        .name = "Hello World Example",
-        .optimize = optimize,
-        .target = target,
-        .root_source_file = .{ .path = "src/example.zig" },
-    });
-    example.linkLibrary(lib);
-
-    const run_example = b.addRunArtifact(example);
-
-    const run_example_step = b.step("example", "Run hello world example");
-    run_example_step.dependOn(&run_example.step);
-
-    //const main_tests = b.addTest(.{
-    //    .root_source_file = .{ .path = "src/main.zig" },
-    //    .target = target,
-    //    .optimize = optimize,
-    //});
-    //main_tests.linkLibrary(lib);
-
-    //const run_main_tests = b.addRunArtifact(main_tests);
-
-    //const test_step = b.step("test", "Run library tests");
-    //test_step.dependOn(&run_main_tests.step);
-}
-
-pub fn addChipmunk(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) *std.Build.Step.Compile {
-    const cp_repo = GitRepoStep.create(b, .{
-        .url = "https://github.com/slembcke/Chipmunk2D",
-        .branch = "master",
-        .sha = "d0239ef4599b3688a5a336373f7d0a68426414ba",
-    });
+    const cp_dep = b.dependency("chipmunk2d", .{});
 
     const lib = b.addStaticLibrary(.{
         .name = "Chipmunk2D-zig",
@@ -47,11 +12,7 @@ pub fn addChipmunk(b: *std.Build, target: std.Build.ResolvedTarget, optimize: st
         .optimize = optimize,
     });
     lib.linkLibC();
-    lib.step.dependOn(&cp_repo.step);
-
-    lib.addIncludePath(.{
-        .path = chipPath(b, &lib.step, cp_repo, &.{"include"}),
-    });
+    lib.addIncludePath(cp_dep.path("include"));
 
     const c_flags_default = &.{
         "-std=gnu99",
@@ -69,7 +30,7 @@ pub fn addChipmunk(b: *std.Build, target: std.Build.ResolvedTarget, optimize: st
         c_flags_default;
 
     lib.addCSourceFiles(.{
-        .root = .{ .path = cp_repo.path },
+        .root = cp_dep.path("."),
         .files = &.{
             "src/chipmunk.c",
             "src/cpArbiter.c",
@@ -107,11 +68,32 @@ pub fn addChipmunk(b: *std.Build, target: std.Build.ResolvedTarget, optimize: st
         .flags = c_flags,
     });
 
-    lib.installHeadersDirectory(chipPath(b, &lib.step, cp_repo, &.{ "include", "chipmunk" }), "chipmunk");
+    lib.installHeadersDirectory(cp_dep.path("include/chipmunk").getPath(b), "chipmunk");
 
-    return lib;
-}
+    b.installArtifact(lib);
 
-fn chipPath(b: *std.Build, step: *const std.Build.Step, cp_repo: *GitRepoStep, comptime paths: []const []const u8) []u8 {
-    return std.fs.path.join(b.allocator, .{cp_repo.getPath(step)} ++ paths) catch @panic("OOM");
+    const example = b.addExecutable(.{
+        .name = "Hello World Example",
+        .optimize = optimize,
+        .target = target,
+        .root_source_file = .{ .path = "src/example.zig" },
+    });
+    example.linkLibrary(lib);
+
+    const run_example = b.addRunArtifact(example);
+
+    const run_example_step = b.step("example", "Run hello world example");
+    run_example_step.dependOn(&run_example.step);
+
+    //const main_tests = b.addTest(.{
+    //    .root_source_file = .{ .path = "src/main.zig" },
+    //    .target = target,
+    //    .optimize = optimize,
+    //});
+    //main_tests.linkLibrary(lib);
+
+    //const run_main_tests = b.addRunArtifact(main_tests);
+
+    //const test_step = b.step("test", "Run library tests");
+    //test_step.dependOn(&run_main_tests.step);
 }
